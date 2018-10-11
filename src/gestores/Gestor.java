4a0f91343a6c0b58a6aeebb41bcb8cb29516327a
rocio <rociosilvaledesma@gmail.com>
2018-10-11 11:51:05 -0300

    comentarios sobre el constructor de juego

--- src/gestores/Gestor.java
+++ src/gestores/Gestor.java
@@ -14,4 +14,23 @@ public class Gestor {
 		
 			
 	}
+	
+	/*
+	 * if (mib==0) {
+			if (ciudades != 0 || bonus city >=1) {
+	 * 			avanzar(){
+					System.out.println("pasa al siguiente nivel"+n);
+					imprimir en pantalla el nro de nivel al que se pasa;
+					
+					volver a generar el random de mbi;
+					resetear los silos;
+					
+					linkedList<ciudad> ciudades=new linkedList<Ciudad>(n,x,y);
+					resetear las ciudades correspondientes(usar los bonus city);
+				}
+		}
+	}
+	
+	*/
+	
 }
