0fcdad167c8922d6e12c0735b953380b0e4d3359
RocioSL <43962282+RocioSL@users.noreply.github.com>
2018-10-13 17:28:50 -0300

    Update Explosion.java

--- src/entes/Explosion.java
+++ src/entes/Explosion.java
@@ -5,32 +5,8 @@ import java.awt.geom.Point2D;
 import javax.swing.*;
 
 public class Explosion extends Entidad {
-
 	
-	/*
-	 * Una explosion se va a dar cuando dos objetos o dos imagenes diferentes se choquen; 
-	 * la posicion de la explosion se va a dar por el cruce de los objetos, del choque me 
-	 * tengo que quedar con la posicion de donde ocurrio y esa va a ser la posicion de la explosion
-	 * 
-	 * si (objeto 1 intersecta objeto 2) >> hay colision:
-	 *  -se genera la imagen de la explosion en pantalla en la ubicacion de la misma.
-	 *  -se saca de pantalla las imagenes de los dos objetos;
-	 * 	-si el objeto 2 es una ciudad:
-	 * 		::se declara como destruida;
-	 * 		
-	 * 	-Si el objeto 2 es un silo:
-	 * 		::se declara como destruido;
-	 * 		::se inhabilita su uso;
-	 * 		
-	 * 	-Si el objeto 2 es un mbi:
-	 * 		::se suman los puntos correspondientes; 
-	 * 
-	 * y esto lo tiene que hacer el gestor no va aca... 
-	 * 
-	 */
-	
-	
-	
+	private Area area;//area va a ser una clase de las librerias la tengo que buscar
 	
 	@Override
 	public Posicion getPosicion() {
@@ -55,6 +31,16 @@ public class Explosion extends Entidad {
 		generarImagen(x,y);
 	}
 
+	public Area getArea() {
+		return area;
+	}//el gestor va a pedir el area para corroborar si en la misma hay o no una colision
+	
+
+	public void setArea(Area area) {
+		this.area = area;
+	}
+	
+	/*
 	private static boolean seChocan(Point2D.Double p1, double r1, Point2D.Double p2, double r2) 
 	{ 
 	    final double a = r1 + r2; 
@@ -64,7 +50,7 @@ public class Explosion extends Entidad {
 	} 
 	
 	
-	/*
+	
 	 * Para ver si un misil choca con un silo o ciudad tendria que usar el alto y ancho de las imagenes
 	 * de cada uno para tener el radio de ubicacion y comparar con el destino final del mbi(si es que 
 	 * no exploto antes con un mba)
